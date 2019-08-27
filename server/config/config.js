ea98a1251da753824cf71fd387c11c22ddecfbd8
felixkiryowa <franciskiryowa68@gmail.com>
2019-08-27 19:01:11 +0300

    Modify config file

--- server/config/config.js
+++ server/config/config.js
@@ -22,7 +22,8 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    use_env_variable: process.env.DATABASE_URL,
+    use_env_variable: 'DATABASE_URL',
+    url: DATABASE_URL,
     "dialect": "postgres"
   }
 }
