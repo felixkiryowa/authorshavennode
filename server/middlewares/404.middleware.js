27c9dd3bc7e654a35642ed2d1a1e9ac49c73b3a1
felixkiryowa <franciskiryowa68@gmail.com>
2019-09-14 17:02:45 +0300

    Removing unwanted files

--- server/middlewares/404.middleware.js
+++
@@ -1,9 +0,0 @@
-import winston from 'winston';
-
-export default (req, res) => {
-    winston.error('Resource not found');
-    return res.status(404).send({
-        status: 404,
-        error: 'Resource not found'
-    });
-};
--- server/middlewares/error.middleware.js
+++
@@ -1,9 +0,0 @@
-import winston from 'winston';
-
-export default (err, req, res) => {
-    winston.error(err.message, err);
-    res.status(500).send({
-        status: 500,
-        error: 'Something failed.'
-    });
-};
