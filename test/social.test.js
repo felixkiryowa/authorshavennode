5c5358c9a6a2da3bb2d85ca57d3b4b7949de3389
felixkiryowa <franciskiryowa68@gmail.com>
2019-09-20 15:48:03 +0300

    Writting social auth tests

---
+++ test/social.test.js
@@ -0,0 +1,11 @@
+import chai from 'chai';
+import app from '../index';
+import chaiHttp from 'chai-http';
+chai.use(chaiHttp);
+const {
+    expect
+} = chai;
+
+describe('Testing social authentication', () => {
+
+});
