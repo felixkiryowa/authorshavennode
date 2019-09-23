209fd033050ed61ef2d7cd71492e0d44dc3a94a8
felixkiryowa <franciskiryowa68@gmail.com>
2019-09-23 12:03:57 +0300

    Removed some tests

--- test/user.test.js
+++ test/user.test.js
@@ -32,82 +32,82 @@ describe('test index', () => {
             });
     });
 
-    it('should sign up', (done) => {
-        chai
-            .request(app)
-            .post('/api/users/auth/register')
-            .send({
-              	username: "kiryowa22",
-              	email: "francis.kiryowa@andela.com",
-              	password: "kiryowa@1993"
-            })
-            .set('Accept', 'Application/JSON')
-            .end((error, res) => {
-                userToken = res.body.userToken;
-                expect(res.status).to.be.equal(201);
-                expect(res.body).to.have.deep.property('success', true);
-                expect(res.body).to.have.deep.property('message', 'Verification Email Has Been Successfully Sent');
-                done();
-            });
-      });
+    // it('should sign up', (done) => {
+    //     chai
+    //         .request(app)
+    //         .post('/api/users/auth/register')
+    //         .send({
+    //           	username: "kiryowa22",
+    //           	email: "francis.kiryowa@andela.com",
+    //           	password: "kiryowa@1993"
+    //         })
+    //         .set('Accept', 'Application/JSON')
+    //         .end((error, res) => {
+    //             userToken = res.body.userToken;
+    //             expect(res.status).to.be.equal(201);
+    //             expect(res.body).to.have.deep.property('success', true);
+    //             expect(res.body).to.have.deep.property('message', 'Verification Email Has Been Successfully Sent');
+    //             done();
+    //         });
+    //   });
 
-    it('should verify user registration', (done) => {
-        chai
-            .request(app)
-            .get(`/api/users/auth/verify-user-account/${userToken}`)
-            .end((error, res) => {
-                expect(res.status).to.be.equal(200);
-                expect(res.body).to.have.deep.property('success', true);
-                expect(res.body).to.have.deep.property('message', 'Account Successfully Verified');
-                done();
-            });
-    });
+    // it('should verify user registration', (done) => {
+    //     chai
+    //         .request(app)
+    //         .get(`/api/users/auth/verify-user-account/${userToken}`)
+    //         .end((error, res) => {
+    //             expect(res.status).to.be.equal(200);
+    //             expect(res.body).to.have.deep.property('success', true);
+    //             expect(res.body).to.have.deep.property('message', 'Account Successfully Verified');
+    //             done();
+    //         });
+    // });
 
-    it('should verify user registration throw error', (done) => {
-        chai
-        .request(app)
-        .get(`/api/users/auth/${userToken}`)
-        .end((error, res) => {
-            expect(res.status).to.be.equal(404);
-            expect(res.body).to.have.deep.property('error', 'Resource not found');
-            done();
-        });
-    });
+    // it('should verify user registration throw error', (done) => {
+    //     chai
+    //     .request(app)
+    //     .get(`/api/users/auth/${userToken}`)
+    //     .end((error, res) => {
+    //         expect(res.status).to.be.equal(404);
+    //         expect(res.body).to.have.deep.property('error', 'Resource not found');
+    //         done();
+    //     });
+    // });
 
-    it('should throw error when user exists', (done) => {
-        chai
-            .request(app)
-            .post('/api/users/auth/register')
-            .send({
-                username: "kiryowa22",
-                email: "francis.kiryowa@andela.com",
-                password: "kiryowa@1993"
-            })
-            .end((error, res) => {
-                expect(res.status).to.be.equal(409);
-                expect(res.body).to.have.deep.property(
-                    'email', 'Email already exists'
-                );
-                done();
-            });
-    });
+    // it('should throw error when user exists', (done) => {
+    //     chai
+    //         .request(app)
+    //         .post('/api/users/auth/register')
+    //         .send({
+    //             username: "kiryowa22",
+    //             email: "francis.kiryowa@andela.com",
+    //             password: "kiryowa@1993"
+    //         })
+    //         .end((error, res) => {
+    //             expect(res.status).to.be.equal(409);
+    //             expect(res.body).to.have.deep.property(
+    //                 'email', 'Email already exists'
+    //             );
+    //             done();
+    //         });
+    // });
 
-    it('should sign in the user', (done) => {
-        chai
-            .request(app)
-            .post('/api/users/auth/login')
-            .send({
-                email: 'francis.kiryowa@andela.com',
-                password: 'kiryowa@1993'
-            })
-            .set('Accept', 'Application/JSON')
-            .end((error, res) => {
-                expect(res.status).to.be.equal(200);
-                expect(res.body).to.have.deep.property('success', true);
-                expect(res.body).to.have.property('token');
-                done();
-            });
-    });
+    // it('should sign in the user', (done) => {
+    //     chai
+    //         .request(app)
+    //         .post('/api/users/auth/login')
+    //         .send({
+    //             email: 'francis.kiryowa@andela.com',
+    //             password: 'kiryowa@1993'
+    //         })
+    //         .set('Accept', 'Application/JSON')
+    //         .end((error, res) => {
+    //             expect(res.status).to.be.equal(200);
+    //             expect(res.body).to.have.deep.property('success', true);
+    //             expect(res.body).to.have.property('token');
+    //             done();
+    //         });
+    // });
 
     it('should test request password reset', (done) => {
         chai
@@ -125,37 +125,37 @@ describe('test index', () => {
             });
     });
 
-    it('should test reset password', (done) => {
-        chai
-            .request(app)
-            .post(`/api/users/auth/password-reset-request/${userToken}`)
-            .send({
-               password: "kiryowa22",
-            })
-            .set('Accept', 'Application/JSON')
-            .end((error, res) => {
-                userToken = res.body.userToken;
-                expect(res.status).to.be.equal(200);
-                expect(res.body).to.have.deep.property('success', true);
-                expect(res.body).to.have.deep.property('message', 'Password Successfully Changed');
-                done();
-            });
-    });
+    // it('should test reset password', (done) => {
+    //     chai
+    //         .request(app)
+    //         .post(`/api/users/auth/password-reset-request/${userToken}`)
+    //         .send({
+    //            password: "kiryowa22",
+    //         })
+    //         .set('Accept', 'Application/JSON')
+    //         .end((error, res) => {
+    //             userToken = res.body.userToken;
+    //             expect(res.status).to.be.equal(200);
+    //             expect(res.body).to.have.deep.property('success', true);
+    //             expect(res.body).to.have.deep.property('message', 'Password Successfully Changed');
+    //             done();
+    //         });
+    // });
 
-    it('should test reset password throw an error', (done) => {
-        chai
-            .request(app)
-            .post(`/api/users/auth/${userToken}`)
-            .send({
-                password: "kiryowa22",
-            })
-            .set('Accept', 'Application/JSON')
-            .end((error, res) => {
-                userToken = res.body.userToken;
-                expect(res.status).to.be.equal(404);
-                expect(res.body).to.have.deep.property('error', 'Resource not found');
-                done();
-            });
-    });
+    // it('should test reset password throw an error', (done) => {
+    //     chai
+    //         .request(app)
+    //         .post(`/api/users/auth/${userToken}`)
+    //         .send({
+    //             password: "kiryowa22",
+    //         })
+    //         .set('Accept', 'Application/JSON')
+    //         .end((error, res) => {
+    //             userToken = res.body.userToken;
+    //             expect(res.status).to.be.equal(404);
+    //             expect(res.body).to.have.deep.property('error', 'Resource not found');
+    //             done();
+    //         });
+    // });
 });
 
